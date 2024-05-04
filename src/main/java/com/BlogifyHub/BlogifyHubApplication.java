9902eaded9bf62e62ab178854618eba5978aec7b
HassanAlwakeel1 <hassanalwakeel911@gmail.com>
2024-05-04 12:37:04 +0300

    Initial commit

---
+++ src/main/java/com/BlogifyHub/BlogifyHubApplication.java
@@ -0,0 +1,13 @@
+package com.BlogifyHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogifyHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogifyHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BlogifyHub/BlogifyHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BlogifyHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogifyHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
